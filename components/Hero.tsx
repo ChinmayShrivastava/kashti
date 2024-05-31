d411897ff0e07627a2132c8e037a2bcd91a94b00
Chinmay Shrivastava <chinmay.shrivastava.1999@gmail.com>
2024-05-30 22:50:32 -0400

    link

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero() {
 				<div className="flex flex-col w-full sm:w-3/5">
 					<h1 className="text-4xl text-center sm:text-left font-normal text-primary py-8">DON’T OVERWHELM YOUR USERS <br /> FOR YOUR RESEARCH.</h1>
 					<Button
-						href="/"
+						href="https://app.kashti.io/1f6af1a5-51b3-484a-bc73-4c6a56594c72"
 						className="w-full h-56"
 					>
 						<p className="text-3xl text-white">
